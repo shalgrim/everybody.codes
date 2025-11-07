cb929778db979562dc19d4f2144434034b6d0c4c
Scott Halgrim <shalgrim@gmail.com>
2025-11-07 12:27:22 -0800

    solidify day1 part 1

--- python/tests/test_day01.py
+++ python/tests/test_day01.py
@@ -1,11 +1,12 @@
 import pytest
-
 from y2025.d01_1 import main
+
 
 @pytest.fixture
 def day01_example_file_lines():
-    with open("data/day01_example.txt") as f:
+    with open("data/2025/day01_example.txt") as f:
         return [line.strip() for line in f.readlines()]
+
 
 def test_part1(day01_example_file_lines):
     assert main(day01_example_file_lines) == "Fyrryn"
--- python/y2025/d01_1.py
+++ python/y2025/d01_1.py
@@ -1,5 +1,6 @@
 from coding_puzzle_tools import read_input
 
+
 def main(lines):
     names = lines[0].split(",")
     directions = lines[2].split(",")
@@ -7,11 +8,12 @@ def main(lines):
     index = 0
     for direction in directions:
         if direction[0] == "R":
-            index = min(len(names)-1, index + int(direction[1]))
+            index = min(len(names) - 1, index + int(direction[1]))
         else:
             index = max(0, index - int(direction[1]))
 
     return names[index]
 
-if __name__ == '__main__':
-    print(read_input())
+
+if __name__ == "__main__":
+    print(main(read_input()))
